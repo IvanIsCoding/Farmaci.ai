4b79578533a6d78d7a709fe65a2acf719e9a6643
Sean Roarty <43792819+sroarty@users.noreply.github.com>
2021-04-02 03:38:46 -0700

    Updated text in notifications page

--- lib/data/patientList.dart
+++ lib/data/patientList.dart
@@ -5,41 +5,41 @@ class PatientList {
   PatientList() {
     list = List(6);
     list[0] = new Patient(
-        "", // name
+        "@bob has a new procedure log", // name
         "Friend", // relationship
-        "Floor 2, Room A",
+        "April 2nd, 2021",
         "She's feeling much better, and will be discharged soon. She ate a full breakfast, walked around the ward several times, and was very talkative and cheerful.",
         "Very Good");
 
     list[1] = new Patient(
-        "@colin", // name
-        "Relative", // relationship
-        "Floor 2, Room B",
+        "@bob has a new prescription drug",
+        "Friend", // relationship
+        "April 2nd, 2021",
         "His breathing and appetite has improved. His spirits are up.",
         "Good, and Improving");
 
     list[2] = new Patient(
-        "@jagan", // name
+        "@bob has a new prescription drug",
         "Friend", // relationship
-        "Floor 2, Room C",
+        "April 2nd, 2021",
         "Her surgery went very well, and she's recovering very well.",
         "Very Good");
     list[3] = new Patient(
-        "@jagan", // name
+        "@bob has a new prescription drug",
         "Friend", // relationship
-        "Floor 2, Room C",
+        "April 2nd, 2021",
         "Her surgery went very well, and she's recovering very well.",
         "Very Good");
     list[4] = new Patient(
-        "@jagan", // name
+        "@bob moved to Floor 2, Room A", // name
         "Friend", // relationship
-        "Floor 2, Room C",
+        "April 2nd, 2021",
         "Her surgery went very well, and she's recovering very well.",
         "Very Good");
     list[5] = new Patient(
-        "@jagan", // name
+        "@bob checked in at Hospital", // name
         "Friend", // relationship
-        "Floor 2, Room C",
+        "April 2nd, 2021",
         "Her surgery went very well, and she's recovering very well.",
         "Very Good");
   }
